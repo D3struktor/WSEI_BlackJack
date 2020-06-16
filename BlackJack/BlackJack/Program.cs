247b79835571e57227e43ac97dc34a370ab47810
D3struktor <40777848+D3struktor@users.noreply.github.com>
2020-06-16 12:44:21 +0200

    1

--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BlackJack
 {
@@ -6,7 +10,56 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int kartyGracza = 0;
+            int kartyKrupiera = 0;
+
+            Random random = new Random();
+
+            kartyGracza += random.Next(1, 12); // Losowa liczba od 1 do 11
+            kartyGracza += random.Next(1, 12); // Losowa liczba od 1 do 11
+
+            kartyKrupiera += random.Next(1, 12); // Losowa liczba od 1 do 11
+            kartyKrupiera += random.Next(1, 12); // Losowa liczba od 1 do 11
+
+            switch (karty)
+            {
+                case 1:
+                case 11:
+                    Console.WriteLine("Ace of Spades");
+                    break;
+                case 2:
+                    Console.WriteLine("Dwójka");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Trójka");
+                    break;
+                case 4:
+                    Console.WriteLine("Czwórka");
+                    break;
+                case 5:
+                    Console.WriteLine("Piątka");
+                    break;
+                case 6:
+                    Console.WriteLine("Szóstka");
+                    break;
+                case 7:
+                    Console.WriteLine("Siódemka");
+                    break;
+                case 8:
+                    Console.WriteLine("Ósemka");
+                    break;
+                case 9:
+                    Console.WriteLine("Dziewiątka");
+                    break;
+                case 10:
+                    Console.WriteLine("Dziesiątka, Walet, Królowa, Król");
+                    break;
+                default:
+                    Console.WriteLine("Joker");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
