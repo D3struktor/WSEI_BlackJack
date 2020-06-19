ef69c21c55c32f3ddc2aa7d15b175ebf58781893
D3struktor <40777848+D3struktor@users.noreply.github.com>
2020-06-19 14:52:10 +0200

    Black Jack v2
    Pytanie o grę i pętla.

--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -8,104 +8,115 @@ namespace BlackJack
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            int kartyGracza = 0;
-            int kartyKrupiera = 0;
-
-            Random random = new Random();
-
-            kartyGracza += random.Next(2, 12); // Losowa liczba od 2 do 11
-            kartyGracza += random.Next(2, 12); // Losowa liczba od 2 do 11
-
-            kartyKrupiera += random.Next(2, 12); // Losowa liczba od 2 do 11
-            Console.WriteLine("Pierwsza karta krupiera to " + kartyKrupiera.ToString());
-            kartyKrupiera += random.Next(2, 12); // Losowa liczba od 2 do 11
-
-            if (kartyGracza > 21)
+            Console.WriteLine("Witaj w kasynie, czy chcialbys zagrac w Black Jack?");
+            Console.WriteLine("1.Gramy\n2.Odchodzisz od stołu");
+            int gra = Convert.ToInt32(Console.ReadLine());
+            while (gra <= 1)
             {
-                kartyGracza -= 10; //zabezpieczenie przed natychmiastową przegraną gracza
-            }
+                
+                int kartyGracza = 0;
+                int kartyKrupiera = 0;
 
-            if (kartyKrupiera > 21)
-            {
-                kartyKrupiera -= 10; //zabezpieczenie przed natychmiastową przegraną krupiera
-            }
+                Random random = new Random();
 
+                kartyGracza += random.Next(2, 12); // Losowa liczba od 2 do 11
+                kartyGracza += random.Next(2, 12); // Losowa liczba od 2 do 11
 
-            //Początek kolejki gracza
-            while (true)
-            {
-                if (kartyGracza == 21)
+                kartyKrupiera += random.Next(2, 12); // Losowa liczba od 2 do 11
+                Console.WriteLine("\n\nPierwsza karta krupiera to " + kartyKrupiera.ToString());
+                kartyKrupiera += random.Next(2, 12); // Losowa liczba od 2 do 11
+
+                if (kartyGracza > 21)
                 {
-                    Console.WriteLine("Jesteś na 21!!");
-                    break;
+                    kartyGracza -= 10; //zabezpieczenie przed natychmiastową przegraną gracza
                 }
-                Console.WriteLine("Jesteś na: " + kartyGracza.ToString() + " Dobierasz?\nT lub N");
 
-                string odpowiedz = Console.ReadLine();
-
-                if (odpowiedz == "Tak" || odpowiedz == "tak" || odpowiedz == "t" || odpowiedz == "T")
+                if (kartyKrupiera > 21)
                 {
-                    kartyGracza += random.Next(2, 12); // Losowa liczba od 2 do 11
-                    if (kartyGracza > 21)
+                    kartyKrupiera -= 10; //zabezpieczenie przed natychmiastową przegraną krupiera
+                }
+
+
+                //Początek kolejki gracza
+                while (true)
+                {
+                    if (kartyGracza == 21)
                     {
-                        Console.WriteLine("Masz za dużo oczek, Mission failed we will get him next time!");
+                        Console.WriteLine("Jesteś na 21!!");
                         break;
                     }
+                    Console.WriteLine("Jesteś na: " + kartyGracza.ToString() + " Dobierasz?\nT lub N");
 
+                    string odpowiedz = Console.ReadLine();
+
+                    if (odpowiedz == "Tak" || odpowiedz == "tak" || odpowiedz == "t" || odpowiedz == "T")
+                    {
+                        kartyGracza += random.Next(2, 12); // Losowa liczba od 2 do 11
+                        if (kartyGracza > 21)
+                        {
+                            Console.WriteLine("Masz za dużo oczek, Mission failed we will get him next time!");
+                            break;
+                        }
+
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+                    else if (odpowiedz == "Nie" || odpowiedz == "nie" || odpowiedz == "n" || odpowiedz == "N")
+                    {
+                        break;
+                    }
                     else
                     {
+                        Console.WriteLine("Niepoprawna odpowiedź, spróbuj jeszcze raz!\nJeśli dobierasz wpisz Tak lub T\nJeśli zostajesz wpisz Nie lub N\n");
                         continue;
                     }
+
                 }
 
-                else if(odpowiedz == "Nie" || odpowiedz == "nie" || odpowiedz == "n" || odpowiedz == "N")
+                //Początek kolejki Krupiera
+                Console.WriteLine("Jesteś na: " + kartyGracza.ToString());
+
+                if (kartyGracza <= 21)
                 {
-                    break;
+                    //Jeśli gracz nie przegrał
+                    while (kartyKrupiera < 21 && kartyKrupiera < kartyGracza)
+                    {
+                        kartyKrupiera += random.Next(2, 12); // Losowa liczba od 2 do 11
+                    }
+                    //Sprawdzamy kto wygrał
+                    if (kartyGracza == kartyKrupiera)
+                    {
+                        Console.WriteLine("Gracz zremisował z krupierem!\n Macie po " + kartyGracza.ToString() + " puntków!");
+                    }
+                    else if (kartyGracza < kartyKrupiera && kartyKrupiera <= 21)
+                    {
+                        Console.WriteLine("Krupier wygrał z " + kartyKrupiera.ToString() + " punktami!");
+                    }
+                    else if (kartyKrupiera > 21)
+                    {
+                        Console.WriteLine("Gracz wygrywa! Krupier wtopił ;)\nWartość kart krupiera: " + kartyKrupiera.ToString());
+                    }
+                    else if (kartyKrupiera == 21)
+                    {
+                        Console.WriteLine("Krupier wygrał z 21 punkatmi!");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Niepoprawna odpowiedź, spróbuj jeszcze raz!\nJeśli dobierasz wpisz Tak lub T\nJeśli zostajesz wpisz Nie lub N\n");
-                    continue;
+                    Console.WriteLine("Krupier wygrał ale wtopa ;/\nWartość kart krupiera: " + kartyKrupiera.ToString());
                 }
-
+                Console.WriteLine("\nKrupier ponawia pytanie");
+                Console.WriteLine("1.Gramy\n2.Odchodzisz od stołu");
+                gra = Convert.ToInt32(Console.ReadLine());
             }
-
-            //Początek kolejki Krupiera
-            Console.WriteLine("Jesteś na: " + kartyGracza.ToString());
-            
-            if (kartyGracza <= 21)
-            {
-                //Jeśli gracz nie przegrał
-                while (kartyKrupiera < 21 && kartyKrupiera < kartyGracza)
-                {
-                   kartyKrupiera += random.Next(2, 12); // Losowa liczba od 2 do 11
-                }
-                //Sprawdzamy kto wygrał
-                if (kartyGracza == kartyKrupiera)
-                {
-                    Console.WriteLine("Gracz zremisował z krupierem!\n Macie po "+kartyGracza.ToString() +" puntków!");
-                }
-                else if (kartyGracza < kartyKrupiera && kartyKrupiera <= 21)
-                {
-                    Console.WriteLine("Krupier wygrał z " + kartyKrupiera.ToString() +" punktami!");
-                }
-                else if (kartyKrupiera > 21)
-                {
-                    Console.WriteLine("Gracz wygrywa! Krupier wtopił ;)\nWartość kart krupiera: " + kartyKrupiera.ToString());
-                }
-                else if (kartyKrupiera == 21)
-                {
-                    Console.WriteLine("Krupier wygrał z 21 punkatmi!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Krupier wygrał ale wtopa ;/\nWartość kart krupiera: " + kartyKrupiera.ToString());
-            }
-   
-            Console.ReadLine();
+            Console.WriteLine("Dziekuje za gre.");
         }
+        
     }
 }
